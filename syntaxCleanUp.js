c4bdcabced038b6a304f8a6f09537e05bfd4311b
Cade Chaney <cadechaney@live.com>
2023-01-12 15:55:54 -0600

    Add corrections to clean up

--- syntaxCleanUp.js
+++ syntaxCleanUp.js
@@ -1,22 +1,33 @@
-var foods = {apples: 23, 
+var foods = {
+  apples: 23, 
   grapes: 507, 
-  eggs: 48}
-
-var person = { height: "6 ft", weight: "160 lbs" }
-
-var post = { imageSrc: "./images/beach.png",
-
-
-   caption: "At the beach with my besties", timestamp: "4:37 PM August 13, 2019",
-        number_likes: 0,
-comments: [] }
+  eggs: 48
+};
+// Drop apples variable down. Put closing curly bracket below eggs. Add semi-colon at end.
+var person = { 
+  height: "6 ft", 
+  weight: "160 lbs" 
+};
 
 
-var post2 =
-{
-      imageSrc: "./images/holiday-party.png",
-      caption: "What a great holiday party omg",
-      timestamp: "11:37 PM December 31, 2019",
-      numberLikes: 13,
-      comments: []
-}
+
+// put keys in order. Drop ending curly bracket to bottom line, add semi-colon.
+var post = { 
+  imageSrc: "./images/beach.png",
+  caption: "At the beach with my besties", 
+  timestamp: "4:37 PM August 13, 2019",
+  number_likes: 0,
+  comments: [] 
+};
+
+//Drop imagesrc key down and bring other keys up in correct placement. Drop ending curly bracket to bottom.
+// Add semi-colon
+var post2 ={
+  imageSrc: "./images/holiday-party.png",
+  caption: "What a great holiday party omg",
+  timestamp: "11:37 PM December 31, 2019",
+  numberLikes: 13,
+  comments: []
+};
+//Put beginning curly bracket next to the equals after variable. Move all keys to correct placement.
+//Add semi-solon at end next to the ending curly bracket.
